45b9e5a372c5f705707c3548ebc5e3012badbbd8
Ievgenii Shepeliuk <ievgenii.shepeliuk@gig.com>
2023-06-21 08:40:30 +0000

    chore: cleanup python after dwh refactoring

--- python/ksqldb_deploy/ksqldb_deploy/deployer/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import KafkaTopicMetadata
-from . import KsqldbDeployer
-from . import KsqldbDeployerContext
-from . import KsqldbScript
--- python/ksqldb_deploy/ksqldb_deploy/util_ksql/KsqldbEntityType.py
+++
@@ -1,7 +0,0 @@
-
-from enum import Enum
-
-class KsqldbEntityType(Enum):
-
-    STREAM = 1
-    TABLE = 2
